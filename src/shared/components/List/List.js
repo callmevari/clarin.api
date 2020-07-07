48432ea354ca61dbcc66ed5834ddedbfc714361e
Vari <molinadiego.95@gmail.com>
2020-07-07 02:31:00 -0300

    List logic moved to List component

--- src/shared/components/List/List.js
+++ src/shared/components/List/List.js
@@ -1,6 +1,65 @@
 import React from 'react';
 
-const List = () => {
+const List = ({ holidays, year }) => {
+
+  const listHolidays = () => {
+    let holidaysArray = [];
+
+    const list = holidays.year.map((each, index) => {
+      if (each.year == year) {
+        
+        for (let key in each) {
+          if (key !== 'year' & key !== '_id') {
+            let row = each[key];
+            holidaysArray.push({
+              day: row.dia,
+              month: row.mes,
+              id: row.id,
+            });
+          }
+        }
+
+        return holidaysArray.map((each, index) => {
+          console.log(each.id);
+          return (
+            <React.Fragment key={index}>
+              <span>{returnMonth(each.month)}</span>
+              <span>{each.day}</span>
+              <span style={{ borderRight: '1px solid black' }}>
+                <a href="#">
+                  <i className='fa fa-eye' style={{ marginRight: '10px' }} /> 
+                </a>
+                <a href="#">
+                  <i className='fa fa-pencil' />
+                </a>
+              </span>
+            </React.Fragment>
+          );
+        })
+      }
+    });
+
+    return list;
+  };
+
+  // move this to the util folder
+  const returnMonth = (month) => {
+    switch(month) {
+      case 1: return 'Enero';
+      case 2: return 'Febrero';
+      case 3: return 'Marzo';
+      case 4: return 'Abril';
+      case 5: return 'Mayo';
+      case 6: return 'Junio';
+      case 7: return 'Julio';
+      case 8: return 'Agosto';
+      case 9: return 'Septiembre';
+      case 10: return 'Octubre';
+      case 11: return 'Noviembre';
+      case 12: return 'Diciembre';
+    }
+  };
+
   return (
     <div className='grid'>
       <span>
@@ -12,6 +71,7 @@ const List = () => {
       <span style={{ borderRight: '1px solid black' }}>
         <strong>Acción</strong>
       </span>
+      {listHolidays()}
     </div>
   );
 };
--- src/shared/components/Select/Select.js
+++ src/shared/components/Select/Select.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Select = ({ text, onChangeHandler, holidays }) => {
   return (
     <div className="form-select">
-      {text}: 
+      <h3>{text}: </h3>
       <select onChange={onChangeHandler}>
         {holidays.year.map(each => <option key={each._id}>{each.year}</option>)}
       </select>
