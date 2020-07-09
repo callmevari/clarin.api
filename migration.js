b57c96d5ee40c6b56a9e3f139d26945f4ca368cf
Vari <molinadiego.95@gmail.com>
2020-07-09 05:00:36 -0300

    migration.js ended

--- migration.js
+++ migration.js
@@ -4,16 +4,39 @@ const axios = require('axios');
 const { DataBaseService } = require('./src/server/services');
 
 class Migration {
-  constructor(){
-    this.years = [2011, 2020];
-    return this.start();
+  constructor() {
+    this.YEAR_BASE = 2011;
+    this.YEAR_LIMIT = process.env.YEAR_LIMIT;
+    return this.migrate();
   }
 
-  async start() {
-    await DataBaseService.connect();
-    console.log(this.years);
-    return console.log('start() is not implemented yet');
-  };
+  async migrate() {
+    let count = 0;
+
+    try {
+      await DataBaseService.connect();
+      const isEmpty = await DataBaseService.getCollections();
+      if (!isEmpty.length) {
+        for (let i = this.YEAR_BASE; i <= this.YEAR_LIMIT; i++) {
+          const holidays = await axios.get(`http://nolaborables.com.ar/api/v2/feriados/${i}?incluir=opcional`);
+          const document = {
+            holidays: holidays.data,
+            year: i
+          };
+          await DataBaseService.insert('holidays', document);
+          count++;
+          console.log(`Document ${count} (${i}) inserted in db`);
+        }
+        console.log(`Process ended`);
+        return await DataBaseService.close();
+      } else {
+        throw new Error('There are existing data in the database already. You need to drop it before start the migration');
+      }
+    } catch (err) {
+      console.error(err);
+      return await DataBaseService.close();
+    }
+  }
 };
 
-const MigrationService = new Migration();
+new Migration();
