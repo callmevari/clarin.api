80678a85137d5fcce29b452bf606d121a147a3d7
Vari <molinadiego.95@gmail.com>
2020-07-13 03:04:35 -0300

    Added favicon to markup

--- src/server/pages/homepage/markup.js
+++ src/server/pages/homepage/markup.js
@@ -7,6 +7,7 @@ module.exports = (jsx, props) => `
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
       <meta name="mobile-web-app-capable" content="yes">
       <link rel="stylesheet" href="/bundle.css">
+      <link rel="shortcut icon" type="image/ico" href="https://comercial.clarin.com/wp-content/uploads/2018/09/favicon.ico"/>
     </head>
     <body>
       <div id="root-app">${jsx}</div>
